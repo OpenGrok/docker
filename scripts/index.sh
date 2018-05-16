86e1069ca60e588898023a48d5951629c1705e89
Andreas Gohr <andi@splitbrain.org>
2018-05-16 18:35:01 +0200

    lock indexing to avoid running multiple at once

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+LOCKFILE=/var/run/opengrok-indexer
+
+if [ -f "$LOCKFILE" ]; then
+    date +"%F %T Indexer still locked, skipping indexing"
+    exit 1
+fi
+
+touch $LOCKFILE
 date +"%F %T Indexing starting"
 /opengrok/bin/OpenGrok index /src
+rm -f $LOCKFILE
 date +"%F %T Indexing finishing"
