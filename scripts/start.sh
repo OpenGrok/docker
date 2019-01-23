acb9f8d604f91a08f3b6b6b7e5384bc555db3b5e
Vladimir Kotal <vlada@devnull.cz>
2019-01-23 14:31:20 +0100

    preserve local modifications to the body include file

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,37 +2,45 @@
 
 # Reindex default
 if [ -z "$REINDEX" ]; then
-    REINDEX=10
+	REINDEX=10
 fi
 
 #START METHOD FOR INDEXING OF OPENGROK
 start_opengrok(){
-    # wait for tomcat startup
-    date +"%F %T Waiting for tomcat startup..."
-    while [ "`curl --silent --write-out '%{response_code}' -o /dev/null 'http://localhost:8080/'`" == "000" ]; do
-        sleep 1;
-    done
-    date +"%F %T Startup finished"
+	# Wait for Tomcat startup.
+	date +"%F %T Waiting for tomcat startup..."
+	while [ "`curl --silent --write-out '%{response_code}' -o /dev/null 'http://localhost:8080/'`" == "000" ]; do
+		sleep 1;
+	done
+	date +"%F %T Startup finished"
 
-    # populate the webapp with bare configuration
-    echo '<p><h1>Waiting on the initial reindex to finish.. Stay tuned !</h1></p>' > /data/body_include
-    /scripts/index.sh --noIndex
-    rm -f /data/body_include
+	# Populate the webapp with bare configuration.
+	BODY_INCLUDE_FILE="/data/body_include"
+	if [[ -f $BODY_INCLUDE_FILE ]]; then
+		mv "$BODY_INCLUDE_FILE" "$BODY_INCLUDE_FILE.orig"
+	fi
+	echo '<p><h1>Waiting on the initial reindex to finish.. Stay tuned !</h1></p>' > "$BODY_INCLUDE_FILE"
+	/scripts/index.sh --noIndex
+	rm -f "$BODY_INCLUDE_FILE"
+	if [[ -f $BODY_INCLUDE_FILE.orig ]]; then
+		mv "$BODY_INCLUDE_FILE.orig" "$BODY_INCLUDE_FILE"
+	fi
 
-    # initial indexing
-    /scripts/index.sh
+	# Perform initial indexing.
+	/scripts/index.sh
+	date +"%F %T Initial reindex finished"
 
-    # continue to index every $REINDEX minutes
-    if [ "$REINDEX" == "0" ]; then
-        date +"%F %T Automatic reindexing disabled"
-        return
-    else
-        date +"%F %T Automatic reindexing in $REINDEX minutes..."
-    fi
-    while true; do
-        sleep `expr 60 \* $REINDEX`
-        /scripts/index.sh
-    done
+	# Continue to index every $REINDEX minutes.
+	if [ "$REINDEX" == "0" ]; then
+		date +"%F %T Automatic reindexing disabled"
+		return
+	else
+		date +"%F %T Automatic reindexing in $REINDEX minutes..."
+	fi
+	while true; do
+		sleep `expr 60 \* $REINDEX`
+		/scripts/index.sh
+	done
 }
 
 #START ALL NECESSARY SERVICES.
