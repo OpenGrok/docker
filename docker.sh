13193e64c32770a952d5ed63ef5b43286bfd77de
Vladimir Kotal <vlada@devnull.cz>
2019-03-27 16:00:50 +0100

    try to use encrypted vars directly

--- docker.sh
+++ docker.sh
@@ -14,8 +14,8 @@ docker ps -a
 VERSION=`curl -sS https://api.github.com/repos/oracle/opengrok/releases/latest | jq -er .tag_name`
 echo "Latest OpenGrok tag: $VERSION"
 
-if [ -n "$DOCKER_PASSWORD" -a -n "$DOCKER_USERNAME" -a -n "$VERSION" ]; then
+#if [ -n "$DOCKER_PASSWORD" -a -n "$DOCKER_USERNAME" -a -n "$VERSION" ]; then
 	echo "Pushing image for version $VERSION"
 	echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 	docker push opengrok/docker:$VERSION
-fi
+#fi
