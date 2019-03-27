5f0de23d93aa11829277100389a4ed6de6414105
Vladimir Kotal <vlada@devnull.cz>
2019-03-27 16:56:58 +0100

    move secure variables to Travis repository settings

--- docker.sh
+++ docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Build and push new image.
+# Build and push new image to Docker hub.
 #
 
 set -e
@@ -14,8 +14,9 @@ docker ps -a
 VERSION=`curl -sS https://api.github.com/repos/oracle/opengrok/releases/latest | jq -er .tag_name`
 echo "Latest OpenGrok tag: $VERSION"
 
-#if [ -n "$DOCKER_PASSWORD" -a -n "$DOCKER_USERNAME" -a -n "$VERSION" ]; then
+# The DOCKER_* variables are setup via https://travis-ci.com/OpenGrok/docker/settings
+if [ -n "$DOCKER_PASSWORD" -a -n "$DOCKER_USERNAME" -a -n "$VERSION" ]; then
 	echo "Pushing image for version $VERSION"
 	echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 	docker push opengrok/docker:$VERSION
-#fi
+fi
