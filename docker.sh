ea139a4a3cc58a4bda473fdadcf6ab6f6c50de17
Vladimir Kotal <vlada@devnull.cz>
2019-03-27 14:48:58 +0100

    print VERSION

--- docker.sh
+++ docker.sh
@@ -12,6 +12,7 @@ docker ps -a
 
 # Get the latest OpenGrok version string.
 VERSION=`curl -sS https://api.github.com/repos/oracle/opengrok/releases/latest | jq -er .tag_name`
+echo "Latest OpenGrok tag: $VERSION"
 
 if [ -n "$DOCKER_PASSWORD" -a -n "$DOCKER_USERNAME" -a -n "$VERSION" ]; then
 	echo "Pushing image for version $VERSION"
